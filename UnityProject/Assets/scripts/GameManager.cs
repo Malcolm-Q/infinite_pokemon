2232296ab0e2e9a5688b55b43cc614095f00e5cb
Malcolm Kew <malcolmthekew@gmail.com>
2023-09-10 19:09:08 -0700

    combat loop implemented

--- UnityProject/Assets/scripts/GameManager.cs
+++ UnityProject/Assets/scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using System.Collections;
 using UnityEngine.Networking;
 using System.Collections.Generic;
@@ -7,20 +8,49 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    [SerializeField] private List<TextMeshProUGUI> playerMoves;
-    [SerializeField] private TextMeshProUGUI attackStatus, enemyName, playerName;
+    [SerializeField] private List<TextMeshProUGUI> playerMoves, playerMoveAttr;
+    [SerializeField] private TextMeshProUGUI attackStatus, enemyName, playerName, loadText, keyStatus;
     [SerializeField] private Image playerPortrait, enemyPortrait, playerHealthBar, enemyHealthBar;
-    [SerializeField] private GameObject loading;
+    [SerializeField] private GameObject loading, getKeyScreen;
     [SerializeField] private OpenAIController openai;
+    [SerializeField] private InputField keyInput;
     private Pokemon playerPokemon, enemyPokemon; 
     private bool playerCanAttack = true;
+    private Dictionary<string, Color> atkTypeColors = new Dictionary<string, Color>();
 
-    private async void Start()
+    private void Start()
     {
         instance = this;
-        // build player and enemy
-        playerPokemon = await openai.PokemonConstructor();
-        enemyPokemon = await openai.PokemonConstructor();
+        atkTypeColors["Fire"] = Color.red;
+        atkTypeColors["Water"] = Color.blue;
+        atkTypeColors["Earth"] = Color.green;
+        atkTypeColors["Wind"] = Color.cyan;
+        atkTypeColors["Normal"] = Color.white;
+    }
+
+    public async void SubmitKey()
+    {
+        bool success = await openai.SetKey(keyInput.text);
+        if(!success){
+            keyStatus.text = "invalid API key!";
+            return;
+            }
+        getKeyScreen.SetActive(false);
+        StartGame();
+    }
+
+    private async void StartGame()
+    {
+        loadText.text = "Constructing Pokemon...";
+        var playerTask = openai.PokemonConstructor();
+        var enemyTask = openai.PokemonConstructor();
+
+        await Task.WhenAll(playerTask, enemyTask);
+
+        playerPokemon = await playerTask;
+        enemyPokemon = await enemyTask;
+
+        loadText.text = "Setting Scene...";
 
         StartCoroutine(LoadImage(playerPokemon.ImgUrl, playerPortrait));
         StartCoroutine(LoadImage(enemyPokemon.ImgUrl, enemyPortrait));
@@ -31,31 +61,52 @@ public class GameManager : MonoBehaviour
         for (int i = 0; i < playerPokemon.MoveSet.Length; i++)
         {
             playerMoves[i].text = playerPokemon.MoveSet[i].MoveName;
+            playerMoveAttr[i].text = playerPokemon.MoveSet[i].MoveType + " // " + playerPokemon.MoveSet[i].MoveDamage.ToString();
+            playerMoveAttr[i].color = atkTypeColors[playerPokemon.MoveSet[i].MoveType];
         }
+        await Task.Delay(500);
+
         loading.SetActive(false);
     }
 
     private async void NewPokemon(bool player, bool enem)
     {
-        if(player)
+        attackStatus.text = "";
+        if(enem)
         {
+            loadText.text = "Constructing New Opponent...";
             enemyPokemon = await openai.PokemonConstructor();
             StartCoroutine(LoadImage(enemyPokemon.ImgUrl, enemyPortrait));
             enemyName.text = enemyPokemon.Name.Trim();
         }
-        if(enem)
+        if(player)
         {
+            loadText.text = "You died :(\nMaking You a New Pokemon...";
             playerPokemon = await openai.PokemonConstructor();
             StartCoroutine(LoadImage(playerPokemon.ImgUrl, playerPortrait));
             playerName.text = playerPokemon.Name.Trim();
+            for (int i = 0; i < playerPokemon.MoveSet.Length; i++)
+            {
+                playerMoves[i].text = playerPokemon.MoveSet[i].MoveName;
+                playerMoveAttr[i].text = playerPokemon.MoveSet[i].MoveType + " // " + playerPokemon.MoveSet[i].MoveDamage.ToString();
+                playerMoveAttr[i].color = atkTypeColors[playerPokemon.MoveSet[i].MoveType];
+            }
         }
+
+        //reset both enemy and player so you can fight until you
+        //meet your match and vice versa.
+        enemyPokemon.Health = enemyPokemon.MaxHealth;
+        enemyHealthBar.transform.localScale = Vector3.one;
+        playerPokemon.Health = playerPokemon.MaxHealth;
+        playerHealthBar.transform.localScale = Vector3.one;
+        await Task.Delay(500);
+
         loading.SetActive(false);
         playerCanAttack=true;
     }
 
     private void PokeAttack(int idx, bool player)
     {
-        Debug.Log(player);
         if(player && playerCanAttack==false){return;}
         else if(player){playerCanAttack=false;}
 
@@ -64,16 +115,26 @@ public class GameManager : MonoBehaviour
         Image health = player ? enemyHealthBar : playerHealthBar;
 
         attackStatus.text = atkPoke.Name + " used " + atkPoke.MoveSet[idx].MoveName + "!"; 
-        defPoke.Health -= atkPoke.MoveSet[idx].MoveDamage;
-
-        Vector3 scale = health.transform.localScale;
-        health.transform.localScale = new Vector3((float)defPoke.Health / (float)defPoke.MaxHealth, scale.y, scale.z);
+        int dmg = atkPoke.MoveSet[idx].MoveDamage;
+        if(atkPoke.MoveSet[idx].MoveType != "Normal")
+        {
+            if(defPoke.PokeType == atkPoke.PokeStrength) {
+                dmg *= 2;
+                attackStatus.text += "\nIt's Super Effective!";    
+            }
+            else if(defPoke.PokeStrength == atkPoke.PokeType) {
+                dmg /= 2;
+                attackStatus.text += "\nIt's Not Very Effective...";    
+            }
+        }
+        defPoke.Health -= dmg;
+        health.transform.localScale = new Vector3((float)defPoke.Health / (float)defPoke.MaxHealth, 1, 1);
 
         if (defPoke.Health <= 0)
         {
             loading.SetActive(true);
-            if(!player){NewPokemon(true, true);}
-            else{NewPokemon(false,true);}
+            if(player){NewPokemon(false, true);}
+            else{NewPokemon(true,false);}
         }
         else{StartCoroutine(WaitForMove(player));}
     }
@@ -89,9 +150,11 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
         attackStatus.text = "";
-        yield return new WaitForSeconds(1f);
-        if(player){PokeAttack(UnityEngine.Random.Range(0,3), false);}
-        else{playerCanAttack=true;}
+        if(!player){playerCanAttack=true;}
+        else{
+            yield return new WaitForSeconds(1f);
+            PokeAttack(UnityEngine.Random.Range(0,3), false);
+        }
     }
     private IEnumerator LoadImage(string imageUrl, Image targetImage)
     {
@@ -107,9 +170,7 @@ public class GameManager : MonoBehaviour
             Texture2D texture = DownloadHandlerTexture.GetContent(www);
 
             Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one * 0.5f);
-            Debug.Log(texture.width);
             targetImage.sprite = sprite;
-            Debug.Log("set");
         }
     }
 }
--- UnityProject/Assets/scripts/OpenAIController.cs
+++ UnityProject/Assets/scripts/OpenAIController.cs
@@ -12,27 +12,58 @@ using OpenAI_API.Images;
 
 public class OpenAIController : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI pokemonName, pokemonMove1, pokemonMove2, pokemonMove3;
     private OpenAIAPI api;
-    [SerializeField] private Image playerImage;
 
+    private string[] pokeTypes = new string[] {
+        "Fire",
+        "Water",
+        "Earth",
+        "Wind",
+        "Normal",
+    };
+
+    private string[] pokeStrengths = new string[]{
+        "Wind",
+        "Earth",
+        "Fire",
+        "Water",
+        "Normal"
+    };
+
+    public async Task<bool> SetKey(string key)
+    {
+        api = new OpenAIAPI(key);
+        await api.Completions.GetCompletion("test");
+        return true;
+    }
     public async Task<Pokemon> PokemonConstructor()
     {
-        api = new OpenAIAPI(Environment.GetEnvironmentVariable("openai_key"));
-        var name = await api.Completions.GetCompletion("generate a name for a random animal that has a random attribute");
-        var result = await api.Completions.GetCompletion("Output you answer with the names seperated by commas. Generate 3 rpg style attack moves that a " + name + " may have.");
-        string[] moves = result.Split(',');
-        var img = await api.ImageGenerations.CreateImageAsync(new ImageGenerationRequest("Pixel art of " + name.Trim(), 1, ImageSize._256));
+        int idx = UnityEngine.Random.Range(0,pokeTypes.Length);
+        string pokeType = pokeTypes[idx];
+        string name = await api.Completions.GetCompletion("Only answer with the name!!! Generate a name for a random " + pokeType + " influenced animal/creature that has a random verb.");
 
-        return new Pokemon(name, MoveSetConstructor(moves), img.Data[0].Url);
+        var moveTask = api.Completions.GetCompletion("Output your answer with the names separated by commas. Generate 3 RPG style attack moves that a " + name + " may have.");
+        var imgTask = api.ImageGenerations.CreateImageAsync(new ImageGenerationRequest("Pixel art of " + name.Trim() + " black background", 1, ImageSize._256));
+
+        await Task.WhenAll(moveTask, imgTask);
+
+        string result = await moveTask;
+        var img = await imgTask;
+
+        string[] moves = result.Split(',');
+
+        return new Pokemon(name, MoveSetConstructor(moves, pokeType), img.Data[0].Url, pokeType, pokeStrengths[idx]);
     }
 
-    private Move[] MoveSetConstructor(string[] moves)
+    private Move[] MoveSetConstructor(string[] moves, string pokeType)
     {
+        string type1 = (UnityEngine.Random.Range(0f,100f) > 66) ? pokeType : "Normal";
+        string type2 = (UnityEngine.Random.Range(0f,100f) > 66) ? pokeType : "Normal";
+        string type3 = (UnityEngine.Random.Range(0f,100f) > 66) ? pokeType : "Normal";
         return new Move[] {
-            new Move(moves[0].Trim(), UnityEngine.Random.Range(10,35)),
-            new Move(moves[1].Trim(), UnityEngine.Random.Range(10,35)),
-            new Move(moves[2].Trim(), UnityEngine.Random.Range(10,35)),
+            new Move(moves[0].Trim(), type1, UnityEngine.Random.Range(10,35)),
+            new Move(moves[1].Trim(), type2, UnityEngine.Random.Range(10,35)),
+            new Move(moves[2].Trim(), type3, UnityEngine.Random.Range(10,35)),
         };
     }
 }
@@ -42,16 +73,20 @@ public class Pokemon
     public int Health {get; set;}
     public string Name {get; set;}
     public string ImgUrl {get; set;}
+    public string PokeType {get; set;}
     public Move[] MoveSet {get; set;}
     public int MaxHealth {get; set;}
+    public string PokeStrength {get; set;}
 
-    public Pokemon(string name, Move[] moveSet, string imgUrl)
+    public Pokemon(string name, Move[] moveSet, string imgUrl, string pokeType, string pokeStrength)
     {
         Health = UnityEngine.Random.Range(80,120);
         MaxHealth = Health;
         Name = name;
         ImgUrl = imgUrl;
         MoveSet = moveSet;
+        PokeType = pokeType;
+        PokeStrength = pokeStrength;
     }
 }
 
@@ -59,11 +94,13 @@ public class Pokemon
 public class Move
 {
     public string MoveName {get; set;}
+    public string MoveType {get; set;}
     public int MoveDamage {get; set;}
 
-    public Move(string moveName, int moveDamage)
+    public Move(string moveName, string moveType, int moveDamage)
     {
         MoveName = moveName;
         MoveDamage = moveDamage;
+        MoveType = moveType;
     }
 }
